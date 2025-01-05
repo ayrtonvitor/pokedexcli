06bd40a572e819f51d5bd1dc9cf3070ee7ee62ff
Ayrton Vitor <ayrtonvitor@pm.me>
2025-01-05 00:56:02 -0300

    implement help command

--- commands.go
+++ commands.go
@@ -11,16 +11,34 @@ type cliCommand struct {
 	callback    func() error
 }
 
-var commands = map[string]cliCommand{
-	"exit": {
-		name:        "exit",
-		description: "Exit the Pokedex\n",
-		callback:    commandExit,
-	},
-}
+var commands map[string]cliCommand
 
 func commandExit() error {
 	fmt.Println("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
 	return nil
+}
+
+func commandHelp() error {
+	fmt.Print("Welcome to the Pokedex!\nUsage:\n\n")
+	for _, v := range commands {
+		fmt.Printf("%s: %s", v.name, v.description)
+	}
+	return nil
+}
+
+func setupCommands() {
+	var commandsDict = map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex\n",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message\n",
+			callback:    commandHelp,
+		},
+	}
+	commands = commandsDict
 }
