47770dc7db2060f9378d04f4803fd1ef0628c1d4
Ayrton Vitor <ayrtonvitor@pm.me>
2025-01-07 22:29:56 -0300

    implement explore command

--- command_explore.go
+++ command_explore.go
@@ -1,7 +1,29 @@
 package main
 
-import "github.com/ayrtonvitor/pokedexcli/internal/pokeapi"
+import (
+	"fmt"
+
+	"github.com/ayrtonvitor/pokedexcli/internal/pokeapi"
+)
 
 func commandExplore(apiClient pokeapi.Client, params ...string) error {
+	if len(params) == 0 {
+		return fmt.Errorf("Explore requires a location name as parameter\n")
+	}
+	loc := params[0]
+	if !apiClient.SearchLocation(loc) {
+		return fmt.Errorf("No such loction %s\n", loc)
+	}
+
+	fmt.Printf("Exploring %s\n", loc)
+	pokemons, err := apiClient.ExploretLocation(loc)
+	if err != nil {
+		return fmt.Errorf("commandExplore: %w", err)
+	}
+
+	fmt.Println("Found Pokemon:", loc)
+	for _, poke := range pokemons {
+		fmt.Printf(" - %s\n", poke)
+	}
 	return nil
 }
--- internal/pokeapi/location.go
+++ internal/pokeapi/location.go
@@ -2,6 +2,7 @@ package pokeapi
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
 	"strconv"
@@ -56,7 +57,11 @@ func (c *Client) getLocations(currentPage int) ([]string, error) {
 	var data locationRes
 	decoder := json.NewDecoder(res.Body)
 	if err := decoder.Decode(&data); err != nil {
-		return nil, fmt.Errorf("Could not decode response\n")
+		return nil, fmt.Errorf("Could not decode response: %v\n", err.Error())
+	}
+
+	if len(data.Results) == 0 {
+		return nil, emptySuccessfulRespError{}
 	}
 
 	locations := []string{}
@@ -71,8 +76,98 @@ func (c *Client) getLocations(currentPage int) ([]string, error) {
 	return locations, nil
 }
 
+func (c *Client) SearchLocation(sup string) bool {
+	var err error = nil
+
+	for i := 1; !errors.Is(emptySuccessfulRespError{}, err); i++ {
+		locs, getErr := c.getLocations(i)
+		err = getErr
+		for _, loc := range locs {
+			if loc == sup {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func (c *Client) ExploretLocation(name string) ([]string, error) {
+	url, err := buildURL(
+		c.config.Url.PokeApiBaseUrl, c.config.Url.Path["location"]+"/"+name, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	if item, ok := c.cache.Get("url"); ok {
+		var values []string
+		err := json.Unmarshal(item, &values)
+		if err == nil {
+			return values, nil
+		}
+	}
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, fmt.Errorf("Could not build request:\n%v\n", err)
+	}
+
+	res, err := c.httpClient.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("Request error:\n%v\n", err)
+	}
+	defer res.Body.Close()
+
+	if !(res.StatusCode >= 200 && res.StatusCode < 300 || res.StatusCode == 304) {
+		return nil, fmt.Errorf("Non-OK HTTP status: %s\n", res.Status)
+	}
+
+	var data locationPokeEncountersRes
+	decoder := json.NewDecoder(res.Body)
+	if err := decoder.Decode(&data); err != nil {
+		return nil, fmt.Errorf("Could not decode response: %v\n", err.Error())
+	}
+
+	if len(data.PokemonEncounters) == 0 {
+		return nil, emptySuccessfulRespError{}
+	}
+
+	pokemons := []string{}
+	for _, poke := range data.PokemonEncounters {
+		pokemons = append(pokemons, poke.Pokemon.Name)
+	}
+	toCache, err := json.Marshal(pokemons)
+	if err == nil {
+		c.cache.Add(url, toCache)
+	}
+
+	return pokemons, nil
+}
+
 type locationRes struct {
 	Results []struct {
 		Name string `json:"name"`
 	} `json:"results"`
+}
+
+type locationPokeEncountersRes struct {
+	PokemonEncounters []struct {
+		Pokemon struct {
+			Name string `json:"name"`
+		}
+	} `json:"pokemon_encounters"`
+}
+
+type emptySuccessfulRespError struct {
+	reqType string
+}
+
+func (e emptySuccessfulRespError) Error() string {
+	switch e.reqType {
+	case "location":
+		return "No more locations\n"
+	case "pokeEncounters":
+		return "Could not find pokemon\n"
+	default:
+		return "Unknown request error\n"
+	}
 }
